13ab57a73cc09b35795745ea988e4e6f1b28cf4f
DPT1004 <phuthanhdo104@gmail.com>
2022-11-22 23:43:38 +0700

    add_employee and change_password

--- src/router/index.js
+++ src/router/index.js
@@ -10,9 +10,9 @@ import HomeAdmin from '@/layouts/HomeAdmin.vue';
 // import DetailAccount from '@/views/admin/employee/DetailAccount.vue';
 
 import Login from '@/views/admin/login/Login.vue';
-// import ChangePassword from '@/views/admin/login/ChangePassword.vue';
-// import Department from '@/views/admin/employee/Department.vue';
-// import Customer from '@/views/admin/employee/Customer.vue';
+import ChangePassword from '@/views/admin/login/ChangePassword.vue';
+import Department from '@/views/admin/employee/Department.vue';
+import Customer from '@/views/admin/employee/Customer.vue';
 
 // import Service from '@/views/admin/service/Service';
 // import ServiceType from '@/views/admin/service/ServiceType';
@@ -42,85 +42,85 @@ const routes = [
       title: 'Home',
     },
     children: [
-    //   {
-    //     path: '/accounts',
-    //     component: Account,
-    //     meta: {
-    //       title: 'Account',
-    //     },
-    //     children: [],
-    //   },
-    //   {
-    //     path: '/accounts/:id',
-    //     component: DetailAccount,
-    //     meta: {
-    //       title: 'Profile',
-    //     },
-    //   },
-    //   {
-    //     path: '/bookings',
-    //     component: Booking,
-    //     meta: {
-    //       title: 'Booking',
-    //     },
-    //   },
-    //   {
-    //     path: '/departments',
-    //     component: Department,
-    //     meta: {
-    //       title: 'Department',
-    //     },
-    //   },
-    //   {
-    //     path: '/customers',
-    //     component: Customer,
-    //     meta: {
-    //       title: 'Customer',
-    //     },
-    //   },
-    //   {
-    //     path: '/services',
-    //     component: Service,
-    //     meta: {
-    //       title: 'Service',
-    //     },
-    //   },
-    //   {
-    //     path: '/services/:id',
-    //     component: DetailService,
-    //     meta: {
-    //       title: 'DetailService',
-    //     },
-    //   },
-    //   {
-    //     path: '/serviceTypes/:id',
-    //     component: ServiceType,
-    //     meta: {
-    //       title: 'ServiceType',
-    //     },
-    //   },
-    //   {
-    //     path: '/discounts',
-    //     component: Discount,
-    //     meta: {
-    //       title: 'Discount',
-    //     },
-    //   },
-    //   {
-    //     path: '/:pathMatch(.*)*',
-    //     component: Error,
-    //     name: 'Error',
-    //     meta: {
-    //       title: 'Error',
-    //     },
-    //   },
-    //   {
-    //     path: 'staffs/:id/password',
-    //     component: ChangePassword,
-    //     meta: {
-    //       title: 'ChangePassword',
-    //     },
-    //   },
+      //   {
+      //     path: '/accounts',
+      //     component: Account,
+      //     meta: {
+      //       title: 'Account',
+      //     },
+      //     children: [],
+      //   },
+      //   {
+      //     path: '/accounts/:id',
+      //     component: DetailAccount,
+      //     meta: {
+      //       title: 'Profile',
+      //     },
+      //   },
+      //   {
+      //     path: '/bookings',
+      //     component: Booking,
+      //     meta: {
+      //       title: 'Booking',
+      //     },
+      //   },
+      //   {
+      //     path: '/departments',
+      //     component: Department,
+      //     meta: {
+      //       title: 'Department',
+      //     },
+      //   },
+      //   {
+      //     path: '/customers',
+      //     component: Customer,
+      //     meta: {
+      //       title: 'Customer',
+      //     },
+      //   },
+      //   {
+      //     path: '/services',
+      //     component: Service,
+      //     meta: {
+      //       title: 'Service',
+      //     },
+      //   },
+      //   {
+      //     path: '/services/:id',
+      //     component: DetailService,
+      //     meta: {
+      //       title: 'DetailService',
+      //     },
+      //   },
+      //   {
+      //     path: '/serviceTypes/:id',
+      //     component: ServiceType,
+      //     meta: {
+      //       title: 'ServiceType',
+      //     },
+      //   },
+      //   {
+      //     path: '/discounts',
+      //     component: Discount,
+      //     meta: {
+      //       title: 'Discount',
+      //     },
+      //   },
+      //   {
+      //     path: '/:pathMatch(.*)*',
+      //     component: Error,
+      //     name: 'Error',
+      //     meta: {
+      //       title: 'Error',
+      //     },
+      //   },
+      //   {
+      //     path: 'staffs/:id/password',
+      //     component: ChangePassword,
+      //     meta: {
+      //       title: 'ChangePassword',
+      //     },
+      //   },
       {
         path: 'sales',
         component: Sales,
@@ -135,20 +135,20 @@ const routes = [
           title: 'Salary',
         },
       },
-    //   {
-    //     path: 'payments',
-    //     component: Payment,
-    //     meta: {
-    //       title: 'Payment',
-    //     },
-    //   },
-    //   {
-    //     path: 'mybooking/:id',
-    //     component: MyBooking,
-    //     meta: {
-    //       title: 'MyBooking',
-    //     },
-    //   },
+      //   {
+      //     path: 'payments',
+      //     component: Payment,
+      //     meta: {
+      //       title: 'Payment',
+      //     },
+      //   },
+      //   {
+      //     path: 'mybooking/:id',
+      //     component: MyBooking,
+      //     meta: {
+      //       title: 'MyBooking',
+      //     },
+      //   },
     ],
   },
 ];
